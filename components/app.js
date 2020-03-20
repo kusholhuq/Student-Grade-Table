a8ed26e0064c5f649025ba08e557c5fc063bd537
Mohsinul Huq <kusholhuq@gmail.com>
2020-03-20 16:45:40 -0700

    Completed rendering the grades

--- components/app.js
+++ components/app.js
@@ -3,11 +3,14 @@ class App{
     console.error(error);
   }
   handleGetGradesSuccess(grades){
-    console.log(grades);
+    //console.log(grades);
+    //can remove this console log later
+    this.gradeTable.updateGrades(grades)
   }
-  constructor(){
+  constructor(gradeTable){
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+    this.gradeTable = gradeTable;
   }
   getGrades(){
     $.ajax({
---
+++ components/grade-table.js
@@ -0,0 +1,27 @@
+class GradeTable{
+  constructor(tableElement){
+    this.tableElement = tableElement;
+  }
+  updateGrades(grades){
+    console.log(grades);
+    var tbody = this.tableElement.querySelector("tbody");
+    tbody.innerHTML = "";
+    for (var i = 0; i<grades.length; i++){
+      var row = document.createElement("tr");
+
+      var name = document.createElement("td");
+      var course = document.createElement("td");
+      var grade = document.createElement("td");
+
+      name.textContent = grades[i].name;
+      course.textContent = grades[i].course;
+      grade.textContent = grades[i].grade;
+
+      row.appendChild(name);
+      row.appendChild(course);
+      row.appendChild(grade);
+
+      tbody.appendChild(row);
+    }
+  }
+}
